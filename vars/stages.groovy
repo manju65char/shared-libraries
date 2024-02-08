6b59ee33c69d3a0fddc5e19317b5bef38b369642
ManjunathAchar <117560065+manju65char@users.noreply.github.com>
2024-02-08 16:36:07 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -6,7 +6,8 @@ def call(String stageName){
      }
   else if ("${stageName}" == "SonarQube Report")
      {
-       sh "mvn clean sonar:sonar"
+       //sh "mvn clean sonar:sonar"
+       
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
